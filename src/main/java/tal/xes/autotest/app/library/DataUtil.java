5b621c8244460b5d90df59969c779fe8c0f8fec9
zhangyazhong <zhangyazhong@100tal.com>
2020-05-09 21:05:33 +0800

    添加工具类以及fastjson和httpclient依赖

---
+++ src/main/java/tal/xes/autotest/app/library/DataUtil.java
@@ -0,0 +1,476 @@
+package tal.xes.autotest.app.library;
+
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+//import org.omg.DynamicAny.NameValuePair;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class DataUtil {
+
+    //String转Json格式
+    public static JSONObject strtojson(String str) throws IOException {
+
+        JSONObject jsonObject;
+        try {
+            jsonObject = JSONObject.parseObject(str);
+            return jsonObject;
+        } catch (Exception e) {
+//            System.out.println("Json解析发生异常");
+            return null;
+        }
+
+    }
+
+    public static JSONArray strtojsonArray(String str) throws IOException {
+        JSONArray jsonArray;
+        try {
+            jsonArray = JSONArray.parseArray(str);
+            return jsonArray;
+        } catch (Exception e) {
+//            System.out.println("JsonArray解析发生异常");
+            return null;
+        }
+
+    }
+
+    public static String getJsonType(String jsonfile) throws IOException {
+        JSONObject jsonObject = strtojson(jsonfile);
+        JSONArray jsonArray = strtojsonArray(jsonfile);
+        if (jsonArray!=null) {
+            return "JSONArray";
+        }
+        else if (jsonObject!=null) {
+            return "JSONObject";
+        }
+        else {
+            return "String";
+        }
+    }
+
+    //传入JsonArray遍历需要找到的key值的对象
+    private static Object get(JSONArray jsonfile, String key) throws IOException {
+        int array_size = jsonfile.size();
+        Object final_object = null;
+        if (array_size>0) {
+            for (int i=0; i<array_size; i++) {
+                JSONObject jsonObject = jsonfile.getJSONObject(i);
+                Object object = get(jsonObject, key);
+                if (object!=null) {
+                    final_object = object;
+                }
+            }
+            return final_object;
+        } else {
+            System.out.println("Error: ----传入的JsonArray为空");
+            return null;
+        }
+    }
+
+    //返回Json文件的Value对象
+    private static Object get(JSONObject jsonfile, String key) throws IOException {
+        Object final_value = null;
+        try {
+            Set<Map.Entry<String, Object>> item_key = jsonfile.entrySet();
+            for (Map.Entry<String, Object> entry : item_key) {//遍历Key list
+                String n_key = entry.getKey();
+                Object value = entry.getValue();
+                if (n_key.equals(key)) {//满足条件则返回value
+                    final_value = value.toString();
+                    break;
+                } else {
+                    if (value instanceof JSONObject) {//校验Value值是否为Json对象
+                        //带入get
+                        Object s = get((JSONObject) value, key);
+                        if (s != null) {
+                            final_value = s;
+                            break;
+                        }
+                    }
+                    else if (value instanceof JSONArray) {//校验Value值是否为JsonArray
+                        if (((JSONArray) value).size() > 0) {
+                            for (int i = 0; i < ((JSONArray) value).size(); i++) {
+                                Object vo = ((JSONArray) value).get(i);
+                                if (vo instanceof JSONObject) {
+                                    JSONObject jsonObject = ((JSONArray) value).getJSONObject(i);
+                                    //带入get
+                                    Object s = get(jsonObject, key);
+                                    if (s != null) {
+                                        final_value = s;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error: ----Json解析失败,获取数据发生异常");
+        }
+        return final_value;
+    }
+
+    //遍历Json数据,通过Key获取Value
+    public static String getValue(JSONObject jsonfile, String key) throws IOException {
+        String final_value = null;
+        Object value = get(jsonfile, key);
+        if (value != null) {
+            final_value = value.toString();
+        }
+        return final_value;
+    }
+
+    //遍历Json数据,通过Key获取Value(传入string类型,支持自动校验json类型)
+    public static String getValue(String jsonfile, String key) throws IOException {
+        String final_value = null;
+        Object value = null;
+        String jsonType = getJsonType(jsonfile);
+        if (jsonType.equals("JSONArray")) {
+            value = get(strtojsonArray(jsonfile), key);
+        }
+        else if (jsonType.equals("JSONObject")) {
+            value = get(strtojson(jsonfile), key);
+        }
+        if (value != null) {
+            final_value = value.toString();
+        }
+        return final_value;
+    }
+
+    //校验Json对象某个Key对应的Value是否为Number类型
+    public static boolean isValueNumber(String jsonfile, String key) throws IOException {
+        boolean final_value = false;
+        String jsonType = getJsonType(jsonfile);
+        if (jsonType.equals("JSONObject")) {
+            JSONObject jsonObject = strtojson(jsonfile);
+            final_value = isValueNumber(jsonObject, key);
+        }
+        else if (jsonType.equals("JSONArray")) {
+            JSONArray jsonArray = strtojsonArray(jsonfile);
+            final_value = isValueNumber(jsonArray, key);
+        }
+        return final_value;
+    }
+
+    public static boolean isValueNumber(JSONObject jsonfile, String key) throws IOException {
+        boolean final_value = false;
+        Object value = get(jsonfile, key);
+        if (value != null) {
+            if (value instanceof Number) {
+                final_value = true;
+            } else {
+                System.out.println("数据类型不为Number");
+            }
+        } else {
+            System.out.println("数据为空,无法校验数据");
+        }
+        return final_value;
+    }
+
+    public static boolean isValueNumber(JSONArray jsonfile, String key) throws IOException {
+        boolean final_value = false;
+        Object value = get(jsonfile, key);
+        if (value != null) {
+            if (value instanceof Number) {
+                final_value = true;
+            } else {
+                System.out.println("数据类型不为Number");
+            }
+        } else {
+            System.out.println("数据为空,无法校验数据");
+        }
+        return final_value;
+    }
+
+    //校验Json对象某个Key对应的Value是否为String类型
+    public static boolean isValueString(String jsonfile, String key) throws IOException {
+        boolean final_value = false;
+        String jsonType = getJsonType(jsonfile);
+        if (jsonType.equals("JSONObject")) {
+            JSONObject jsonObject = strtojson(jsonfile);
+            final_value = isValueString(jsonObject, key);
+        }
+        else if (jsonType.equals("JSONArray")) {
+            JSONArray jsonArray = strtojsonArray(jsonfile);
+            final_value = isValueString(jsonArray, key);
+        }
+        return final_value;
+    }
+
+    public static boolean isValueString(JSONObject jsonfile, String key) throws IOException {
+        boolean final_value = false;
+        Object value = get(jsonfile, key);
+        if (value != null) {
+            if (value instanceof String) {
+                final_value = true;
+            } else {
+                System.out.println("数据类型不为String");
+            }
+        } else {
+            System.out.println("数据为空,无法校验数据");
+        }
+        return final_value;
+    }
+
+    public static boolean isValueString(JSONArray jsonfile, String key) throws IOException {
+        boolean final_value = false;
+        Object value = get(jsonfile, key);
+        if (value != null) {
+            if (value instanceof String) {
+                final_value = true;
+            } else {
+                System.out.println("数据类型不为String");
+            }
+        } else {
+            System.out.println("数据为空,无法校验数据");
+        }
+        return final_value;
+    }
+
+    //校验Json对象某个Key对应的Value是否为Boolean类型
+    public static boolean isValueBoolean(String jsonfile, String key) throws IOException {
+        boolean final_value = false;
+        String jsonType = getJsonType(jsonfile);
+        if (jsonType.equals("JSONObject")) {
+            JSONObject jsonObject = strtojson(jsonfile);
+            final_value = isValueBoolean(jsonObject, key);
+        }
+        else if (jsonType.equals("JSONArray")) {
+            JSONArray jsonArray = strtojsonArray(jsonfile);
+            final_value = isValueBoolean(jsonArray, key);
+        }
+        return final_value;
+    }
+
+    public static boolean isValueBoolean(JSONObject jsonfile, String key) throws IOException {
+        boolean final_value = false;
+        Object value = get(jsonfile, key);
+        if (value != null) {
+            if (value instanceof Boolean) {
+                final_value = true;
+            } else {
+                System.out.println("数据类型不为Boolean");
+            }
+        } else {
+            System.out.println("数据为空,无法校验数据");
+        }
+        return final_value;
+    }
+
+    public static boolean isValueBoolean(JSONArray jsonfile, String key) throws IOException {
+        boolean final_value = false;
+        Object value = get(jsonfile, key);
+        if (value != null) {
+            if (value instanceof Boolean) {
+                final_value = true;
+            } else {
+                System.out.println("数据类型不为Boolean");
+            }
+        } else {
+            System.out.println("数据为空,无法校验数据");
+        }
+        return final_value;
+    }
+
+    //校验Json对象某个Key对应的Value是否为Array类型
+    public static boolean isValueArray(String jsonfile, String key) throws IOException {
+        boolean final_value = false;
+        String jsonType = getJsonType(jsonfile);
+        if (jsonType.equals("JSONObject")) {
+            JSONObject jsonObject = strtojson(jsonfile);
+            final_value = isValueArray(jsonObject, key);
+        }
+        else if (jsonType.equals("JSONArray")) {
+            JSONArray jsonArray = strtojsonArray(jsonfile);
+            final_value = isValueArray(jsonArray, key);
+        }
+        return final_value;
+    }
+
+    public static boolean isValueArray(JSONObject jsonfile, String key) throws IOException {
+        boolean final_value = false;
+        Object value = get(jsonfile, key);
+        if (value != null) {
+            System.out.println(value.getClass().toString());
+            if (value instanceof JSONArray) {
+                final_value = true;
+            } else {
+                System.out.println("数据类型不为Array");
+            }
+        } else {
+            System.out.println("数据为空,无法校验数据");
+        }
+        return final_value;
+    }
+
+    public static boolean isValueArray(JSONArray jsonfile, String key) throws IOException {
+        boolean final_value = false;
+        Object value = get(jsonfile, key);
+        if (value != null) {
+            System.out.println(value.getClass().toString());
+            if (value instanceof JSONArray) {
+                final_value = true;
+            } else {
+                System.out.println("数据类型不为Array");
+            }
+        } else {
+            System.out.println("数据为空,无法校验数据");
+        }
+        return final_value;
+    }
+
+    //校验Json对象某个Key对应的Value是否为Object类型
+    public static boolean isValueObject(String jsonfile, String key) throws IOException {
+        boolean final_value = false;
+        String jsonType = getJsonType(jsonfile);
+        if (jsonType.equals("JSONObject")) {
+            JSONObject jsonObject = strtojson(jsonfile);
+            final_value = isValueObject(jsonObject, key);
+        }
+        else if (jsonType.equals("JSONArray")) {
+            JSONArray jsonArray = strtojsonArray(jsonfile);
+            final_value = isValueObject(jsonArray, key);
+        }
+        return final_value;
+    }
+
+    public static boolean isValueObject(JSONObject jsonfile, String key) throws IOException {
+        boolean final_value = false;
+        Object value = get(jsonfile, key);
+        if (value != null) {
+            System.out.println(value.getClass().toString());
+            if (value instanceof JSONObject) {
+                final_value = true;
+            } else {
+                System.out.println("数据类型不为Object");
+            }
+        } else {
+            System.out.println("数据为空,无法校验数据");
+        }
+        return final_value;
+    }
+
+    public static boolean isValueObject(JSONArray jsonfile, String key) throws IOException {
+        boolean final_value = false;
+        Object value = get(jsonfile, key);
+        if (value != null) {
+            System.out.println(value.getClass().toString());
+            if (value instanceof JSONObject) {
+                final_value = true;
+            } else {
+                System.out.println("数据类型不为Object");
+            }
+        } else {
+            System.out.println("数据为空,无法校验数据");
+        }
+        return final_value;
+    }
+
+    //从map中获取数据重新定义为Headers中的Cookie
+    public String mapToCookie(HashMap<String, String> map) throws IOException {
+        try {
+            StringBuffer stringBuffer = new StringBuffer();
+                for (Map.Entry<String, String> entry : map.entrySet()) {
+                    String key = entry.getKey();
+                    String value = entry.getValue();
+                    stringBuffer.append(key).append("=").append(value).append(";");
+                }
+            return stringBuffer.toString();
+
+        } catch (Exception e) {
+            System.out.println("Error : Map to Cookie");
+            return null;
+        }
+
+    }
+
+
+    //map转list
+    public List<NameValuePair> mapToList(HashMap<String, String> map) throws IOException {
+        try {
+            List<NameValuePair> data = new ArrayList<NameValuePair>();
+            for (Map.Entry<String, String> entry : map.entrySet()) {
+                data.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
+            }
+            return data;
+        } catch (Exception e) {
+            System.out.println("Error : Map to List");
+            return null;
+        }
+    }
+
+    //处理form-data传参中带有对象或列表的参数
+    private void paramsUtil(String key, String value, List<NameValuePair> data) throws IOException {
+        String jsonType = getJsonType(value);
+        String final_value = null;
+        StringBuffer start_key = new StringBuffer();
+        start_key.append(key);
+        if (jsonType.equals("JSONArray")) {
+            JSONArray jsonArray = strtojsonArray(value);
+            int jsonArray_lenth = jsonArray.size();
+            for (int i=0; i<jsonArray_lenth; i++) {
+                StringBuffer final_key = new StringBuffer();
+                final_key.append(start_key.toString());
+                final_key.append("[").append(i).append("]");
+                paramsUtil(final_key.toString(), jsonArray.get(i).toString(), data);
+            }
+        } else if (jsonType.equals("JSONObject")) {
+            JSONObject jsonObject = strtojson(value);
+            Set<Map.Entry<String, Object>> item_key = jsonObject.entrySet();
+            for (Map.Entry<String, Object> entry : item_key) {//遍历Key list
+                StringBuffer final_key = new StringBuffer();
+                String n_key = entry.getKey();
+                final_key.append(start_key.toString());
+                final_key.append(".");
+                final_key.append(n_key);
+                String n_value = entry.getValue().toString();
+                paramsUtil(final_key.toString(), n_value, data);
+            }
+        } else if (jsonType.equals("String")) {
+            final_value = value;
+            BasicNameValuePair basicNameValuePair = new BasicNameValuePair(start_key.toString(), final_value);
+            data.add(basicNameValuePair);
+        }
+    }
+
+    //根据传入字符串的格式,校验数据类型
+    public static String getTypeByStr(String data) throws IOException {
+        String flag;
+        if (DataUtil.strtojson(data)!=null) {
+            flag = "JSONObject";
+        } else if (DataUtil.strtojsonArray(data) != null) {
+            flag = "JSONArray";
+        } else {
+            flag = "String";
+        }
+        return flag;
+    }
+
+    //根据传入字符串的格式,校验数据类型
+    public static String getListSize(String data) throws IOException {
+        String jsonType = getJsonType(data);
+        if (jsonType.equals("JSONArray")) {
+            JSONArray jsonArray = DataUtil.strtojsonArray(data);
+            if (jsonArray!=null) {
+                return String.valueOf(jsonArray.size());
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    //获取当前时间戳
+    public static String getRandom() {
+        DateFormat dateTimeFormat = new SimpleDateFormat("yyMMddHHmmssSS");
+        String id = dateTimeFormat.format(new Date());
+        return id;
+    }
+}
---
+++ src/main/java/tal/xes/autotest/app/library/httpUtil.java
@@ -0,0 +1,252 @@
+package tal.xes.autotest.app.library;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.serializer.PropertyFilter;
+import org.apache.http.Consts;
+import org.apache.http.Header;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class httpUtil {
+
+    private static CloseableHttpResponse response;
+    private static CloseableHttpClient httpClient;
+
+    //get请求基础方法
+    private static CloseableHttpResponse get(String url, HashMap<String, String> map, HashMap<String, String> headers) {
+        try {
+            httpClient = HttpClients.createDefault();
+            RequestConfig requestConfig = RequestConfig.custom().setSocketTimeout(20000).setConnectTimeout(20000).build();
+            StringBuffer params = new StringBuffer();
+            String final_url = null;
+            if (map.size()!=0) {
+                int i = 0;
+                for (Map.Entry<String, String> entry : map.entrySet()) {
+                    if (i == 0) {
+                        params.append("?");
+                    } else {
+                        params.append("&");
+                    }
+                    String key = entry.getKey();
+                    String value = entry.getValue();
+                    params.append(key).append("=").append(value);
+                    i++;
+                }
+                final_url = url + params.toString();
+            } else {
+                final_url = url;
+            }
+            HttpGet httpGet = new HttpGet(final_url);
+            if (headers != null) {
+                for (Map.Entry<String, String> entry : headers.entrySet()) {//get请求中的Headers
+                    String key = entry.getKey();
+                    String value = entry.getValue();
+                    httpGet.setHeader(key, value);
+                }
+            }
+            httpGet.setConfig(requestConfig);
+            response = httpClient.execute(httpGet);
+            return response;
+        } catch (IOException ex) {
+            System.out.println("Get请求失败");
+            return null;
+        }
+    }
+
+    //get请求获取status code
+    public static String getStatusCode(String url, HashMap<String, String> map) throws IOException {
+        String statuscode = null;
+        CloseableHttpResponse rp = get(url, map, null);
+        if (rp != null) {
+            int sc = rp.getStatusLine().getStatusCode();
+            statuscode = String.valueOf(sc);
+            response.close();
+            httpClient.close();
+        }
+        return statuscode;
+    }
+
+    public static String getStatusCode(String url, HashMap<String, String> map, HashMap<String, String> headers) throws IOException {
+        String statuscode = null;
+        CloseableHttpResponse rp = get(url, map, headers);
+        if (rp != null) {
+            int sc = rp.getStatusLine().getStatusCode();
+            statuscode = String.valueOf(sc);
+            response.close();
+            httpClient.close();
+        }
+        return statuscode;
+    }
+
+    //get请求获取接口响应数据
+    public static String getResponse(String url, HashMap<String, String> datamap) throws IOException {
+        String responsedata = null;
+        CloseableHttpResponse rp = get(url, datamap, null);
+        if (rp != null) {
+            responsedata = EntityUtils.toString(rp.getEntity(), "utf-8");
+            response.close();
+            httpClient.close();
+        }
+        return setFilter(responsedata);
+    }
+    public static String getResponse(String url, HashMap<String, String> datamap, HashMap<String, String> headers) throws IOException {
+        String responsedata = null;
+        CloseableHttpResponse rp = get(url, datamap, headers);
+        if (rp != null) {
+            responsedata = EntityUtils.toString(rp.getEntity(), "utf-8");
+            response.close();
+            httpClient.close();
+        }
+        return setFilter(responsedata);
+    }
+
+    //post请求基础方法
+    private static CloseableHttpResponse post(String url, HashMap<String, String> data, HashMap<String, String> headers, HashMap<String, String> cookies) throws IOException {
+        try {
+            httpClient = HttpClients.createDefault();
+            RequestConfig requestConfig = RequestConfig.custom().setSocketTimeout(20000).setConnectTimeout(20000).build();
+            HttpPost httpPost = new HttpPost(url);
+            DataUtil dataUtil = new DataUtil();
+            UrlEncodedFormEntity entity = new UrlEncodedFormEntity(dataUtil.mapToList(data), Consts.UTF_8);//post请求中的data数据
+            httpPost.setConfig(requestConfig);
+            httpPost.setEntity(entity);
+            if (headers != null) {
+                for (Map.Entry<String, String> entry : headers.entrySet()) {//post请求中的Headers
+                    String key = entry.getKey();
+                    String value = entry.getValue();
+                    httpPost.setHeader(key, value);
+                }
+            }
+            if (cookies != null) {
+                String c = dataUtil.mapToCookie(cookies);
+                httpPost.setHeader("Cookie", c);
+            }
+            response = httpClient.execute(httpPost);
+            return response;
+        } catch (IOException ex) {
+            System.out.println("Post请求失败");
+            return null;
+        }
+    }
+
+    //Post请求得到Status code
+    public static String postStatusCode(String url, HashMap<String, String> data, HashMap<String, String> headers) throws IOException {
+        String statuscode = null;
+        CloseableHttpResponse rp = post(url, data, headers, null);
+        if (rp != null) {
+            int sc = rp.getStatusLine().getStatusCode();
+            statuscode = String.valueOf(sc);
+            response.close();
+            httpClient.close();
+        }
+        return statuscode;
+    }
+
+    public static String postStatusCode(String url, HashMap<String, String> data) throws IOException {
+        String statuscode = null;
+        CloseableHttpResponse rp = post(url, data, null, null);
+        if (rp != null) {
+            int sc = rp.getStatusLine().getStatusCode();
+            statuscode = String.valueOf(sc);
+            response.close();
+            httpClient.close();
+        }
+        return statuscode;
+    }
+
+    //Post请求得到Status code
+    public static String postStatusCode(String url, HashMap<String, String> data, HashMap<String, String> headers, HashMap<String, String> cookies) throws IOException {
+        String statuscode = null;
+        CloseableHttpResponse rp = post(url, data, headers, cookies);
+        if (rp != null) {
+            int sc = rp.getStatusLine().getStatusCode();
+            statuscode = String.valueOf(sc);
+            response.close();
+            httpClient.close();
+        }
+        return statuscode;
+    }
+
+
+    //POST请求获取接口响应数据
+    public static String postResponse(String url, HashMap<String, String> data, HashMap<String, String> headers) throws IOException {
+        String responsedata = null;
+        CloseableHttpResponse rp = post(url, data, headers, null);
+        if (rp != null) {
+            responsedata = EntityUtils.toString(rp.getEntity(), "utf-8");
+            response.close();
+            httpClient.close();
+        }
+        return setFilter(responsedata);
+    }
+
+    //get请求获取接口响应数据
+    public static String postResponse(String url, HashMap<String, String> data, HashMap<String, String> headers, HashMap<String, String> cookies) throws IOException {
+        String responsedata = null;
+        CloseableHttpResponse rp = post(url, data, headers, cookies);
+        if (rp != null) {
+            responsedata = EntityUtils.toString(rp.getEntity(), "utf-8");
+            response.close();
+            httpClient.close();
+        }
+        return setFilter(responsedata);
+    }
+
+    //POST请求获取接口响应数据
+    public static String postResponse(String url, HashMap<String, String> data) throws IOException {
+        String responsedata = null;
+        CloseableHttpResponse rp = post(url, data, null, null);
+        if (rp != null) {
+            responsedata = EntityUtils.toString(rp.getEntity(), "utf-8");
+            response.close();
+            httpClient.close();
+        }
+        return setFilter(responsedata);
+    }
+
+    //POST请求获取Headers
+    public static Header[] postHeaders(String url, HashMap<String, String> data) throws IOException {
+        Header[] responseHeaders = new Header[0];
+        CloseableHttpResponse rp = post(url, data, null, null);
+        if (rp != null) {
+            responseHeaders = rp.getAllHeaders();
+            response.close();
+            httpClient.close();
+        }
+        System.out.println(responseHeaders);
+        return responseHeaders;
+    }
+
+
+    private static String setFilter(String responsedata) {
+        JSONObject jsonObject = JSON.parseObject(responsedata);
+        return JSON.toJSONString(jsonObject, HttpUtil.filter);
+    }
+
+    private static PropertyFilter filter = new PropertyFilter() {
+        @Override
+        public boolean apply(Object object, String name, Object value) {
+
+            if("traceId".equals(name)){
+                return false;
+            }
+            if("version".equals(name)){
+                return false;
+            }
+            return true;
+        }
+    };
+
+}
